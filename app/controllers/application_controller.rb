e7101b14c6a8e7b884419eabf7e8bf67549c29b5
pmezamoc <pmezamoc@chicagobooth.edu>
2023-03-04 21:09:21 +0000

    graphs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,39 @@ class ApplicationController < ActionController::Base
   end
 
   def offer_calculation
+    the_plan = CompensationPlan.new
+
+    the_plan.stock_type  = cookies.fetch(:stock_type) 
+    the_plan.strike_price  = cookies.fetch(:strike_price) 
+    the_plan.vesting_years = cookies.fetch(:vesting_years) 
+    the_plan.cliff = cookies.fetch(:cliff) 
+    the_plan.number_of_options = cookies.fetch(:number_of_options) 
+
+    the_plan.save
+
+    #vesting calendar
+    @vesting_month = Array.new
+    @vested_stock = Array.new
+    starting_month = the_plan.cliff * 12
+    @vesting_month.push(starting_month)
+    @vested = the_plan.number_of_options * (0.25)
+    @vested_stock.push(@vested)
+
+    @months = (the_plan.vesting_years * 12) - starting_month
+    current_month = starting_month + 1
+    vest_per_month = ((the_plan.number_of_options * (0.75)) / @months)
+    counter = 1
+
+    while counter <= @months
+
+        @vested = @vested + vest_per_month
+        @vesting_month.push(current_month)
+        @vested_stock.push(@vested.round)
+        current_month = current_month + 1
+        counter = counter + 1
+    end
+    @new_array = @vested_stock.map.with_index(starting_month){|i, starting_month| [starting_month, i]}
+
     @offer_value = cookies[:number_of_options].to_i ** cookies[:strike_price].to_i
     @taxes = @offer_value ** 0.16
     render({:template => "results/offer_results"})
