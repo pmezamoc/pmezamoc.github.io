91b4438d80159d1304e6468c358011df91d72716
pmezamoc <pmezamoc@chicagobooth.edu>
2023-03-05 20:59:22 +0000

    git

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,7 @@ class ApplicationController < ActionController::Base
   def offer_calculation
     the_plan = CompensationPlan.new
 
+    the_plan.employee_id = session[:client_id] 
     the_plan.stock_type  = cookies.fetch(:stock_type) 
     the_plan.strike_price  = cookies.fetch(:strike_price) 
     the_plan.vesting_years = cookies.fetch(:vesting_years) 
@@ -47,9 +48,19 @@ class ApplicationController < ActionController::Base
     @vesting_month = Array.new
     @vested_stock = Array.new
     starting_month = the_plan.cliff * 12
+    counter = 1
+
     @vesting_month.push(starting_month)
     @vested = the_plan.number_of_options * (0.25)
-    @vested_stock.push(@vested)
+
+    while counter <= starting_month
+      if counter < starting_month
+        @vested_stock.push(0)
+      else 
+        @vested_stock.push(@vested)
+      end
+      counter = counter + 1 
+    end
 
     @months = (the_plan.vesting_years * 12) - starting_month
     current_month = starting_month + 1
@@ -57,17 +68,109 @@ class ApplicationController < ActionController::Base
     counter = 1
 
     while counter <= @months
-
         @vested = @vested + vest_per_month
-        @vesting_month.push(current_month)
-        @vested_stock.push(@vested.round)
-        current_month = current_month + 1
-        counter = counter + 1
+        @vested_stock.push(@vested.round) 
+        counter = counter + 1 
     end
-    @new_array = @vested_stock.map.with_index(starting_month){|i, starting_month| [starting_month, i]}
 
-    @offer_value = cookies[:number_of_options].to_i ** cookies[:strike_price].to_i
-    @taxes = @offer_value ** 0.16
+    @vesting_calendar = @vested_stock.map.with_index(1){|i, ind| [ind, i]}
+
+    #2Y scenario 
+    years = Array.new
+    years = ["2x","5x","10x"]
+    @potential_upside = Array.new
+    @taxes = Array.new
+    @cost_to_exercise = Array.new
+    
+    @stocks_vested = @vesting_calendar.select{|(x, y)| x==the_plan.vesting_years*12}.map{|(x, y)| y}.join.to_i
+    @cost_to_exercise.push(@stocks_vested * -the_plan.strike_price)
+    @taxes.push((@stocks_vested * -the_plan.strike_price) * 0.20)
+    @potential_upside.push(-(@stocks_vested * -the_plan.strike_price) + ((@stocks_vested * -the_plan.strike_price) * 0.20))
+
+    @stocks_vested = @vesting_calendar.select{|(x, y)| x==the_plan.vesting_years*12}.map{|(x, y)| y}.join.to_i
+    @cost_to_exercise.push(@stocks_vested * -the_plan.strike_price)
+    @taxes.push((@stocks_vested * -the_plan.strike_price * 5) * 0.20)
+    @potential_upside.push(-(@stocks_vested * -the_plan.strike_price * 5) + ((@stocks_vested * -the_plan.strike_price * 5) * 0.20))
+
+    @stocks_vested = @vesting_calendar.select{|(x, y)| x == the_plan.vesting_years*12}.map{|(x, y)| y}.join.to_i
+    @cost_to_exercise.push(@stocks_vested * -the_plan.strike_price)
+    @taxes.push((@stocks_vested * -the_plan.strike_price * 10) * 0.20)
+    @potential_upside.push(-(@stocks_vested * -the_plan.strike_price * 10) + ((@stocks_vested * -the_plan.strike_price * 10) * 0.20))
+
+    @cost_to_exercise = @cost_to_exercise.zip(years).map(&:reverse)
+    @potential_upside = @potential_upside.zip(years).map(&:reverse)
+    @taxes = @taxes.zip(years).map(&:reverse)
+    render({:template => "results/offer_results"})
+  end
+
+  def recalculate
+    the_plan = CompensationPlan.new
+
+    the_plan.employee_id = session[:client_id] 
+    the_plan.stock_type  = params.fetch("query_stock_type") 
+    the_plan.strike_price  = params.fetch("query_strike_price") 
+    the_plan.vesting_years = params.fetch("query_vesting_years") 
+    the_plan.cliff = params.fetch("query_cliff") 
+    the_plan.number_of_options = params.fetch("query_number_of_options") 
+
+    the_plan.save
+
+    #vesting calendar
+    @vesting_month = Array.new
+    @vested_stock = Array.new
+    starting_month = the_plan.cliff * 12
+    counter = 1
+
+    @vesting_month.push(starting_month)
+    @vested = the_plan.number_of_options * (0.25)
+
+    while counter <= starting_month
+      if counter < starting_month
+        @vested_stock.push(0)
+      else 
+        @vested_stock.push(@vested)
+      end
+      counter = counter + 1 
+    end
+
+    @months = (the_plan.vesting_years * 12) - starting_month
+    current_month = starting_month + 1
+    vest_per_month = ((the_plan.number_of_options * (0.75)) / @months)
+    counter = 1
+
+    while counter <= @months
+        @vested = @vested + vest_per_month
+        @vested_stock.push(@vested.round) 
+        counter = counter + 1 
+    end
+
+    @vesting_calendar = @vested_stock.map.with_index(1){|i, ind| [ind, i]}
+
+    #2Y scenario 
+    years = Array.new
+    years = ["2x","5x","10x"]
+    @potential_upside = Array.new
+    @taxes = Array.new
+    @cost_to_exercise = Array.new
+    
+    @stocks_vested = @vesting_calendar.select{|(x, y)|x== the_plan.vesting_years*12}.map{|(x, y)| y}.join.to_i
+    @cost_to_exercise.push(@stocks_vested * -the_plan.strike_price)
+    @taxes.push((@stocks_vested * -the_plan.strike_price) * 0.20)
+    @potential_upside.push(-(@stocks_vested * -the_plan.strike_price) + ((@stocks_vested * -the_plan.strike_price) * 0.20))
+
+    @stocks_vested = @vesting_calendar.select{|(x, y)|x== the_plan.vesting_years*12}.map{|(x, y)| y}.join.to_i
+    @cost_to_exercise.push(@stocks_vested * -the_plan.strike_price)
+    @taxes.push((@stocks_vested * -the_plan.strike_price * 5) * 0.20)
+    @potential_upside.push(-(@stocks_vested * -the_plan.strike_price * 5) + ((@stocks_vested * -the_plan.strike_price * 5) * 0.20))
+
+    @stocks_vested = @vesting_calendar.select{|(x, y)| x == the_plan.vesting_years*12}.map{|(x, y)| y}.join.to_i
+    @cost_to_exercise.push(@stocks_vested * -the_plan.strike_price)
+    @taxes.push((@stocks_vested * -the_plan.strike_price * 10) * 0.20)
+    @potential_upside.push(-(@stocks_vested * -the_plan.strike_price * 10) + ((@stocks_vested * -the_plan.strike_price * 10) * 0.20))
+
+    @cost_to_exercise = @cost_to_exercise.zip(years).map(&:reverse)
+    @potential_upside = @potential_upside.zip(years).map(&:reverse)
+    @taxes = @taxes.zip(years).map(&:reverse)
     render({:template => "results/offer_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
 
 
   # Routes for the Compensation plan homeflow:
+  post("/recalculate", {:controller => "application", :action => "recalculate"})
   post("/store_offer_cookies", {:controller => "compensation_plans", :action => "store_offer_cookies"})
+ 
   get("/", {:controller => "application", :action => "start"})
   get("/begin_service", {:controller => "application", :action => "begin_service"})
+
   get("/value_offer", {:controller => "application", :action => "value_offer"})
   get("/value_stock", {:controller => "application", :action => "value_stock"})
+  
   
   # Routes for the Compensation plan calculation:
   get("/offer_calculation", {:controller => "application", :action => "offer_calculation" })
